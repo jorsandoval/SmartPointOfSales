a58a2c828368d5cc047cb460f468d74edd52ae75
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-06-20 18:51:55 -0400

    avance

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
+
+def home(request):
+    clientes = Cliente.objects.all()
+    datos = {
+        'clientes': clientes
+    }
+    return render(request, 'core/form_cliente.html',datos)
