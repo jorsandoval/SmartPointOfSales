bdaf29e479db826cb263804715e3cde10bd740b7
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-06-21 19:38:49 -0400

    datos

---
+++ Codea_tu_nota.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+datos = {
+    'Nombre' :  'Curso de Python',
+    'Dia' :  'Jueves 30 de Junio',
+    'Hora' :  '17:00 horas',
+    'Plataforma' : 'Microsoft Teams',
+    'Inscripción' : 3.990,
+    'Descripcion' : 'Se acerca el examen de Python y este curso es el que necesitas. '
+        + 'Inscríbete y se parte de la experiencia programando un juego simple.'
+}
+
+
+
+___
+
+
+
+
+
