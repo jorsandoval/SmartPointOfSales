dbddd78926ed91a38e1b06f6a7ef5e73d9a2de2a
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-06-26 12:40:06 -0400

    Avance en mantenedor de modelos y Nav

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, home_cliente, home_detalle_venta, home_medio_pago, home_producto, home_venta, form_cliente, form_venta, form_producto, form_mediopago
+from .views import form_mod_producto, home, home_cliente, home_detalle_venta, home_medio_pago, home_producto, home_venta, form_cliente, form_venta, form_producto, form_mediopago, form_detalle_venta, form_mod_cliente, form_mod_producto
 
 
 urlpatterns = [
@@ -12,5 +12,8 @@ urlpatterns = [
     path('form_cliente',form_cliente, name="form_cliente"),
     path('form_venta',form_venta, name="form_venta"),
     path('form_producto',form_producto, name="form_producto"),
+    path('form_detalle_venta', form_detalle_venta, name="form_detalle_venta"),
     path('form_mediopago', form_mediopago, name="form_mediopago"),
+    path('form_mod_cliente/<id_cliente>', form_mod_cliente, name="form_mod_cliente"),
+    path('form_mod_producto/<id_producto>', form_mod_producto, name="form_mod_producto"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from http import client
 from django.shortcuts import render
 from .models import Cliente, DetalleVenta, MedioPago, Producto, Venta
 from .forms import ClienteForm, VentaForm, ProductoForm, DetalleVentaForm, MedioPagoForm
@@ -42,35 +43,85 @@ def home_venta(request):
     }
     return render(request, 'core/home_venta.html', datos)
 
-#Formularios
+#Formularios de ingreso
 
 def form_cliente(request):
     datos = {
         'form': ClienteForm()
     }
+    if request.method == 'POST':
+        formulario = ClienteForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Cliente guardado correctamente"
     return render(request, 'core/form_cliente.html',datos)
 
 def form_venta(request):
     datos = {
         'form': VentaForm()
     }
+    if request.method == 'POST':
+        formulario = VentaForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje']="Venta registrada Correctamente"
     return render(request, 'core/form_venta.html',datos)
 
 def form_producto(request):
     datos = {
         'form': ProductoForm()
     }
+    if request.method == 'POST':
+        formulario = ProductoForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Producto registrado correctamente"
     return render(request, 'core/form_producto.html',datos)    
 
-def form_producto(request):
+def form_detalle_venta(request):
     datos = {
         'form': DetalleVentaForm()
     }
-    return render(request, 'core/form_producto.html',datos)  
+    if request.method == 'POST':
+        formulario = DetalleVentaForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Detalle de venta registrado correctamente"
+    return render(request, 'core/form_detalle_venta.html',datos)  
 
 def form_mediopago(request):
     datos = {
         'form': MedioPagoForm()
     }
+    if request.method == 'POST':
+        formulario = MedioPagoForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje']= "Medio de pago registrado correctamente"
     return render(request, 'core/form_medio_pago.html',datos)
 
+#Formularios de modificación
+
+def form_mod_cliente(request,id_cliente):
+    cliente = Cliente.objects.get(id_cliente=id_cliente)
+    datos = {
+        'form': ClienteForm(instance=cliente)
+    }
+    if request.method == 'POST':
+        formulario = ClienteForm(data=request.POST,instance=cliente)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Cliente modificado correctamente"
+    return render(request, 'core/form_mod_cliente.html',datos)
+
+def form_mod_producto(request,id_producto):
+    producto = Producto.objects.get(id_producto=id_producto)
+    datos = {
+        'form': ProductoForm(instance=producto)
+    }
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje']= "Producto modificado correctamente"
+    return render(request, 'core/form_mod_producto.html', datos)
